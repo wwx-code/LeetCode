fdaac8b9f03606f56fe9188979e037006c218901
weiweixiong <weiweixiong@sunline.cn>
2021-03-10 08:42:14 +0800

    两数相加

---
+++ LeetCode/src/com/wwxyh/March/subject02.java
@@ -0,0 +1,62 @@
+package com.wwxyh.March;
+
+public class subject02 {
+    /**
+     * 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，
+     * 并且每个节点只能存储 一位 数字。请你将两个数相加，并以相同形式返回一个表示和的链表。
+     * 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+     *
+     * 示例1：
+     *      输入：l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+     *      输出：[8,9,9,9,0,0,0,1]
+     *      解释：9999999 + 9999 = 1000998.
+     * 示例2：
+     *      输入：l1 = [1,2,3], l2 = [4,5,6]
+     *      输出：[5,7,9]
+     *      解释：321 + 654 = 975.
+     */
+    public class ListNode {
+      int val;
+      ListNode next;
+
+      ListNode() {}
+
+      ListNode(int val) {
+          this.val = val;
+      }
+
+      ListNode(int val, ListNode next) {
+          this.val = val; this.next = next;
+      }
+  }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        //初始化 p 和 q 分别为给定的两个链表 l1 和 l2 的头，也就是个位。
+        ListNode p = l1,q = l2,curr = dummyHead;
+        //进位
+        int carry = 0;
+        //循环，直到 l1 和 l2 全部到达 null 。
+        while (p != null || q != null){
+            //设置 x 为 p 节点的值，如果 p 已经到达了 null，设置 x 为 0
+            int x = (p != null) ? p.val : 0;
+            //设置 y 为 q 节点的值，如果 q 已经到达了 null，设置 y 为 0
+            int y = (q != null) ? q.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            curr.next = new ListNode(sum % 10);
+            curr = curr.next;
+            if (p != null){
+                p = p.next;
+            }
+            if (q != null){
+                q = q.next;
+            }
+        }
+        if (carry > 0){
+            curr.next = new ListNode(carry);
+        }
+        return dummyHead.next;
+    }
+
+}
