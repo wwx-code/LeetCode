15b88b6e85874d09f82121ebd3c8aea67babfc82
weiweixiong <1092486722@qq.com>
2021-05-27 21:09:07 +0800

    移除元素

---
+++ LeetCode/src/com/wwxyh/March/subject27.java
@@ -0,0 +1,45 @@
+package com.wwxyh.March;
+
+/**
+ * @Description: 移除元素
+ * @Author: wwx
+ * @Date: 2021/5/27 20:42
+ */
+public class subject27 {
+
+    /**
+     * 给你一个数组 nums 和一个值 val，你需要 原地 移除所有数值等于 val 的元素，并返回移除后数组的新长度。
+     * 不要使用额外的数组空间，你必须仅使用 O(1) 额外空间并 原地 修改输入数组。
+     * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
+     *
+     *
+     * 输入：nums = [3,2,2,3], val = 3
+     * 输出：2, nums = [2,2]
+     * 解释：函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。你不需要考虑数组中超出新长度后面的元素。例如，函数返回的新长度为 2 ，而 nums = [2,2,3,3] 或 nums = [2,2,0,0]，也会被视作正确答案。
+     *
+     *
+     * 输入：nums = [0,1,2,2,3,0,4,2], val = 2
+     * 输出：5, nums = [0,1,4,0,3]
+     * 解释：函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。注意这五个元素可为任意顺序。你不需要考虑数组中超出新长度后面的元素。
+     **/
+
+    public static int removeElement(int[] nums, int value){
+        int len = nums.length;
+        //左指针left指向下一个将要赋值的位置
+        int left = 0;
+        //右指针right指向当前将要处理的元素
+        for (int right = 0; right < len; right++) {
+            if (value != nums[right]){
+                nums[left] = nums[right];
+                left++;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,2,3,6,4};
+        int result = removeElement(arr, 1);
+        System.out.println("移除后数组长度:" + result);
+    }
+}
