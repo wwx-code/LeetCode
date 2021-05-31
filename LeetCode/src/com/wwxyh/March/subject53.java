1f99d4b5d62cbd8a6f54b7152f3b4c88bfcf7fed
weiweixiong <1092486722@qq.com>
2021-05-31 17:04:37 +0800

    最大子序和

--- LeetCode/src/com/wwxyh/March/subject53.java
+++ LeetCode/src/com/wwxyh/March/subject53.java
@@ -7,6 +7,17 @@ package com.wwxyh.March;
  */
 public class subject53 {
 
+    /*
+     * 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和
+     *
+     * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+     * 输出：6
+     * 解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
+     *
+     * 输入：nums = [-1]
+     * 输出：-1
+     **/
+
     //贪心算法求解
     public static int maxSumGreedy(int[] arr){
         //最大和
