88cc4e3349240593506f5152740a67eddcb310f4
weiweixiong <1092486722@qq.com>
2021-07-10 21:28:40 +0800

    二叉树的最大深度

---
+++ LeetCode/src/com/wwxyh/PrimaryAlgorithms/Tree/subject01.java
@@ -0,0 +1,54 @@
+package com.wwxyh.PrimaryAlgorithms.Tree;
+
+import java.util.Stack;
+
+/**
+ * @Description: 二叉树的最大深度
+ * @Author: wwx
+ * @Date: 2021/7/10 20:58
+ */
+public class subject01 {
+
+    /**
+     * 给定一个二叉树，找出其最大深度。
+     * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+     * 说明: 叶子节点是指没有子节点的节点。
+     *
+     * 给定二叉树 [3,9,20,null,null,15,7]，
+     *
+     *     3
+     *    / \
+     *   9  20
+     *     /  \
+     *    15   7
+     **/
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //递归
+    public int maxDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left,right) + 1;
+    }
+}
